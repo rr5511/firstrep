29668eeb9d669c836d96a1e01f598fc8ac15d1c6
Mahmoud <rr5511@yahoo.com>
2019-03-14 00:45:13 +0400

    last

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ session_start();
 if  (!isset($_SESSION['LOG_NAME'])){  header( "location:../pmain/" );echo '<div style=" text-align: center ; font-size:xx-large;color: white ;  background: #FFCCFF   "> <a href="../pmain/" style="text-decoration: none ;">نرجو تسجيل الدخول</a></div> ';}  else {
 echo header("location:index210.php");
 }
+
+
 ?>
